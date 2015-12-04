697382dc8e58cb61c34fefce9665f588990e5c06
goliatone <burgosemi@gmail.com>
2015-12-03 22:22:50 -0500

    boot calls app.before_setup

--- boot.lua
+++ boot.lua
@@ -1,5 +1,8 @@
 -- Create a global variable holding our app
 app = dofile("app.lua")
+if app.before_setup then
+    app.before_setup()
+end
 
 -- Create a global variable holding config
 config = dofile("config.lua")
