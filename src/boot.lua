fe530f0df45331d492e4895bb1a1eec7303207a2
goliatone <burgosemi@gmail.com>
2016-02-28 21:50:59 -0500

    boot: update uart setup baude rate

--- src/boot.lua
+++ src/boot.lua
@@ -1,3 +1,7 @@
+-- If the device panics and resets at any time,
+-- errors will be written to the serial interface at 115200 bps.
+uart.setup(0, 115200, 8, 0, 1, 1)
+
 -- Create a global variable holding our app
 app = dofile("app.lua")
 if app.before_setup then
